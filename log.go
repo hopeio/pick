58014c2ad4e7afe47f1c6f12ad8ef1b747be4a70
贾一饼 <lby.i@qq.com>
2024-04-12 06:23:06 +0800

    chore: 依赖升级

--- log.go
+++ log.go
@@ -1,14 +1,14 @@
 package pick
 
 import (
-	"fmt"
-	"github.com/hopeio/tiga/utils/console/concolor"
+	"github.com/hopeio/tiga/utils/console/style"
+	"github.com/hopeio/tiga/utils/log"
 	stringsi "github.com/hopeio/tiga/utils/strings"
 )
 
 func Log(method, path, title string) {
-	fmt.Printf(" %s\t %s %s\t %s\n",
-		concolor.Green("API:"),
-		concolor.Yellow(stringsi.FormatLen(method, 6)),
-		concolor.Blue(stringsi.FormatLen(path, 50)), concolor.Purple(title))
+	log.Printf(" %s\t %s %s\t %s",
+		style.Green("API:"),
+		style.Yellow(stringsi.FormatLen(method, 6)),
+		style.Blue(stringsi.FormatLen(path, 50)), style.Purple(title))
 }

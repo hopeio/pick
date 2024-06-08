fa4047420f080d7e7ac3e26861acba2d6cadba57
贾一饼 <lby.i@qq.com>
2024-06-08 23:45:23 +0800

    fix: 依赖升级

--- log.go
+++ log.go
@@ -11,5 +11,5 @@ func Log(method, path, title string) {
 	log.Printf(" %s\t %s %s\t %s",
 		style.Green("API:"),
 		style.Yellow(stringsi.FormatLen(method, 6)),
-		style.Blue(stringsi.FormatLen(path, 50)), style.Purple(title))
+		style.Blue(stringsi.FormatLen(path, 50)), style.Magenta(title))
 }

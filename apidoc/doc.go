f23758c3d09d5dca7b2444b1e2740682fd37b0ca
贾一饼 <lby.i@qq.com>
2025-11-10 14:08:02 +0800

    fix: apidoc

--- apidoc/doc.go
+++ apidoc/doc.go
@@ -7,16 +7,12 @@
 package apidoc
 
 import (
-	"encoding/json"
 	"net/http"
-	"os"
-	"path/filepath"
 	"reflect"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/hopeio/gox/log"
 	"github.com/hopeio/gox/net/http/apidoc"
-	"github.com/hopeio/gox/reflect/structtag"
 	"github.com/hopeio/pick"
 )
 
@@ -45,14 +41,19 @@ func RegisterApiInfo(apiInfo *GroupApiInfo) {
 	groupApiInfos = append(groupApiInfos, apiInfo)
 }
 
+func Openapi(filePath, modName string) {
+	doc := GetDoc(modName)
+	apidoc.WriteToFile(filePath, modName, doc)
+}
+
 var Doc *openapi3.T
 
 // 参数为路径和格式
-func GetDoc(realPath, modName string) *openapi3.T {
+func GetDoc(modName string) *openapi3.T {
 	if Doc != nil {
 		return Doc
 	}
-	api := NewAPI(modName)
+	api := apidoc.NewAPI(modName)
 	for _, groupApiInfo := range groupApiInfos {
 		for _, methodInfo := range groupApiInfo.Infos {
 			GenOpenapi(methodInfo, api, groupApiInfo.Describe)
@@ -66,102 +67,20 @@ func GetDoc(realPath, modName string) *openapi3.T {
 	return Doc
 }
 
-func WriteToFile(realPath, modName string) {
-	if Doc == nil {
-		return
-	}
-	if realPath == "" {
-		realPath = "."
-	}
-
-	realPath = realPath + modName
-	err := os.MkdirAll(realPath, os.ModePerm)
-	if err != nil {
-		log.Error(err)
-	}
-
-	realPath = filepath.Join(realPath, modName+apidoc.OpenapiEXT)
-
-	if _, err := os.Stat(realPath); err == nil {
-		os.Remove(realPath)
-	}
-	var file *os.File
-	file, err = os.Create(realPath)
-	if err != nil {
-		log.Error(err)
-	}
-	defer file.Close()
-
-	enc := json.NewEncoder(file)
-	enc.SetIndent("", "  ")
-	err = enc.Encode(Doc)
-	if err != nil {
-		log.Error(err)
-	}
-
-	/*b, err := yaml.Marshal(swag.ToDynamicJSON(Doc))
-	  if err != nil {
-	  	log.Error(err)
-	  }
-	  if _, err := file.Write(b); err != nil {
-	  	log.Error(err)
-	  }*/
-
-	Doc = nil
-}
-
-func Openapi(filePath, modName string) {
-	GetDoc(filePath, modName)
-	WriteToFile(filePath, modName)
-}
-
-func GenOpenapi(methodInfo *ApiDocInfo, api *API, dec string) {
+func GenOpenapi(methodInfo *ApiDocInfo, api *apidoc.API, dec string) {
 	for _, route := range methodInfo.ApiInfo.Routes {
 		r := api.Route(route.Method, route.Path)
 		numIn := methodInfo.Method.NumIn()
 		if numIn == 3 {
-			InType := methodInfo.Method.In(2).Elem()
-			for j := 0; j < InType.NumField(); j++ {
-				tags, err := structtag.Parse(string(InType.Field(j).Tag))
-				if err != nil {
-					return
-				}
-				if uri := tags.MustGet("uri"); uri.Value != "" && uri.Value != "-" {
-					r.HasPathParameter(uri.Value, PathParam{
-						Description:       tags.MustGet("comment").Value,
-						Regexp:            "",
-						Type:              Type(InType.Field(j).Type),
-						ApplyCustomSchema: nil,
-					})
-				}
-				if uri := tags.MustGet("path"); uri.Value != "" && uri.Value != "-" {
-					r.HasPathParameter(uri.Value, PathParam{
-						Description:       tags.MustGet("comment").Value,
-						Regexp:            "",
-						Type:              Type(InType.Field(j).Type),
-						ApplyCustomSchema: nil,
-					})
-				}
-				if query := tags.MustGet("query"); query.Value != "" && query.Value != "-" {
-					r.HasQueryParameter(query.Value, QueryParam{
-						Description:       tags.MustGet("comment").Value,
-						Regexp:            "",
-						Type:              Type(InType.Field(j).Type),
-						ApplyCustomSchema: nil,
-					})
-				}
-				if uri := tags.MustGet("json"); uri.Value != "" && uri.Value != "-" {
-					r.HasRequestModel(Model{Type: InType})
-				}
-			}
+			r.HasRequest(apidoc.Model{Type: methodInfo.Method.In(2).Elem()})
 		}
 		r.HasTags([]string{dec, methodInfo.ApiInfo.Title + route.Remark})
 		if methodInfo.Method.Out(0).Implements(pick.ErrorType) {
-			r.HasResponseModel(http.StatusOK, Model{Type: pick.ErrRepType})
+			r.HasResponseModel(http.StatusOK, apidoc.Model{Type: pick.ErrRepType})
 		} else {
-			r.HasResponseModel(http.StatusOK, Model{Type: methodInfo.Method.Out(0)})
+			r.HasResponseModel(http.StatusOK, apidoc.Model{Type: methodInfo.Method.Out(0)})
 		}
-		r.HasResponseModel(http.StatusBadRequest, Model{Type: pick.ErrRepType})
+		r.HasResponseModel(http.StatusBadRequest, apidoc.Model{Type: pick.ErrRepType})
 
 	}
 }
--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ toolchain go1.24.0
 require (
 	github.com/getkin/kin-openapi v0.132.0
 	github.com/gin-gonic/gin v1.10.0
-	github.com/hopeio/context v0.5.1
-	github.com/hopeio/gox v1.3.4
+	github.com/hopeio/context v0.5.2
+	github.com/hopeio/gox v1.3.6
 )
 
 require (
